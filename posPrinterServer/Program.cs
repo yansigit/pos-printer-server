a773a356dc7742232f286755049791aeb475623a
yansigit <holmes9595@mail.ulsan.ac.kr>
2021-04-08 01:56:08 +0900

    reprint blueprint

--- posPrinterServer/Program.cs
+++ posPrinterServer/Program.cs
@@ -123,6 +123,20 @@ namespace posPrinterServer
                                 Console.WriteLine("프린트가 사용중입니다. 타 앱에서 프린트를 사용 종료 후 다시 실행하세요.");
                             }
                         }
+                        else if (json["action"].ToString() == "reprint")
+                        {
+                            try
+                            {
+                                Console.WriteLine("영수증 재출력을 시작합니다");
+                                // 긴 프린트 하는 함수로 변경
+                                StartLongPrint(json);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                                Console.WriteLine("프린트가 사용중입니다. 타 앱에서 프린트를 사용 종료 후 다시 실행하세요.");
+                            }
+                        }
                         else
                         {
                             Console.WriteLine("액션: " + json["action"].ToString());
@@ -197,7 +211,111 @@ namespace posPrinterServer
             printer.Dispose();
         }
 
-        // 프린트 함수
+        // 다시 프린트
+        static void StartLongPrint(JObject json)
+        {
+            //프린터 연결
+#if DEBUG
+            printer = new TestSerialPrinter(portName: "COM1", baudRate: 9600);
+#else
+            printer = new SerialPrinter(portName: "COM1", baudRate: 9600);
+#endif
+
+            // 메뉴 해쉬코드 - 내용 딕셔너리
+            Dictionary<int, JToken> menuDictionary = new Dictionary<int, JToken>();
+
+            // 각 메뉴마다 수량 겟하고 내용 저장
+            foreach (var menu in json["menus"])
+            {
+                int menuHash = menu.ToString().GetHashCode();
+
+                if (menuDictionary.ContainsKey(menuHash))
+                    menuDictionary[menuHash]["quantity"] = menuDictionary[menuHash]["quantity"].ToObject<int>() + 1;
+                else
+                {
+                    menu["quantity"] = 1;
+
+                    var option = menu["options"].First;
+
+                    // 옵션 확인하고 수량 없으면 삭제
+                    while (option != null)
+                    {
+                        var currentOption = option;
+                        option = currentOption.Next;
+                        if (currentOption["quantity"].ToObject<int>() <= 0)
+                            currentOption.Remove();
+                    }
+
+                    menuDictionary.Add(menuHash, menu);
+                }
+            }
+
+            string orderNum = json["orderNum"].ToString();
+
+            printer.Write(
+              ByteSplicer.Combine(
+                e.FeedLines(1),
+                e.CenterAlign(),
+                e.SetStyles(PrintStyle.FontB | PrintStyle.DoubleHeight | PrintStyle.DoubleWidth | PrintStyle.Bold),
+                e.PrintLine("주문번호: " + orderNum),
+                e.SetStyles(PrintStyle.None),
+                e.PrintLine("--------------------------"),
+                e.PrintLine("11호관 커피숍"),
+                e.PrintLine(System.DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")),//시간
+                e.PrintLine("--------------------------")
+              ));
+
+            byte[] bodyBytes = e.FeedLines(1);
+            foreach (var menu in menuDictionary.Values)
+            {
+                string menu_pojang = menu["isTakeOut"].Value<bool>() ? "포장" : "테이블";
+                string menu_name = menu["name"].ToString();
+                string menu_tumbler = menu["isTumbler"].Value<bool>() ? "텀블러" : null;
+                string menu_temp = menu["temp"].ToString(); // 아이스, 핫
+                int menu_quantity = menu["quantity"].ToObject<int>();
+
+                // (온도) 메뉴이름 (포장여부)
+                string menu_name_line = "(" + menu_temp + ") " + menu_name + " (" + menu_pojang + ")";
+
+                byte[] menuBytes = ByteSplicer.Combine(
+                    e.SetStyles(PrintStyle.FontB | PrintStyle.DoubleHeight | PrintStyle.DoubleWidth | PrintStyle.Bold),
+                    e.PrintLine(menu_name_line),
+                    e.PrintLine("수량 : " + menu_quantity)
+                );
+                if (menu["options"].Any())
+                {
+                    menuBytes = ByteSplicer.Combine(menuBytes,
+                        e.SetStyles(PrintStyle.FontB | PrintStyle.DoubleHeight | PrintStyle.DoubleWidth)
+                    // , e.PrintLine("옵션")
+                    );
+                    JArray optionsArray = menu["options"].ToObject<JArray>();
+
+                    foreach (var option in optionsArray)
+                    {
+                        menuBytes = ByteSplicer.Combine(menuBytes,
+                            e.PrintLine(option["name"].ToString() + " : " + option["quantity"].ToString())
+                        );
+                    }
+                }
+
+                bodyBytes = ByteSplicer.Combine(bodyBytes, menuBytes, e.FeedLines(1));
+            }
+
+            printer.Write(bodyBytes);
+
+            printer.Write(
+                ByteSplicer.Combine(e.FeedLines(3)));
+
+            printer.Write(
+                ByteSplicer.Combine(
+                    e.FullCut()
+                )
+            );
+            //프린터 해제
+            printer.Dispose();
+        }
+
+        // 직원용프린트 함수
         static void StartPrint(JObject json)
         {
             //프린터 연결
